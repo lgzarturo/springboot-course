932ced13e5d82f777fff3371af1de7a1a5f81752
Arturo Lopez <lgzarturo@gmail.com>
2026-03-08 18:29:49 -0500

    refactor: actualizar nomenclatura de anotaciones en pruebas
    - Corrige imports de anotaciones de prueba para alinearse con la nueva nomenclatura
    - Cambia WebMvcTest, DataJpaTest y AutoConfigureTestDatabase a sus equivalentes actualizados
    - Ajusta configuración de ObjectMapper para usar herramientas actualizadas

--- src/test/kotlin/com/lgzarturo/springbootcourse/hotels/integration/e2e/HotelE2ETest.kt
+++ src/test/kotlin/com/lgzarturo/springbootcourse/hotels/integration/e2e/HotelE2ETest.kt
@@ -11,9 +11,9 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase
+import org.springframework.boot.resttestclient.TestRestTemplate
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
--- src/test/kotlin/com/lgzarturo/springbootcourse/shared/config/ObjectMapperConfig.kt
+++ src/test/kotlin/com/lgzarturo/springbootcourse/shared/config/ObjectMapperConfig.kt
@@ -1,9 +1,9 @@
 package com.lgzarturo.springbootcourse.shared.config
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
+import tools.jackson.databind.ObjectMapper
+import tools.jackson.module.kotlin.jacksonObjectMapper
 
 @TestConfiguration
 class ObjectMapperConfig {
