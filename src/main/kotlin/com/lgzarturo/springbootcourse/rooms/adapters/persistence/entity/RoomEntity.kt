e7375a27c9a4e250d3fd4a0ed23d7a0859c04234
Arturo Lopez <lgzarturo@gmail.com>
2026-04-03 18:51:15 -0500

    refactor: ordenar imports en RoomEntity
    - Reorganiza los imports para seguir un orden lógico y consistente
    - Mejora la claridad del código al ajustar la posición de las anotaciones

--- src/main/kotlin/com/lgzarturo/springbootcourse/rooms/adapters/persistence/entity/RoomEntity.kt
+++ src/main/kotlin/com/lgzarturo/springbootcourse/rooms/adapters/persistence/entity/RoomEntity.kt
@@ -2,13 +2,13 @@ package com.lgzarturo.springbootcourse.rooms.adapters.persistence.entity
 
 import com.lgzarturo.springbootcourse.hotels.adapters.persistence.entity.HotelEntity
 import jakarta.persistence.Entity
-import jakarta.persistence.Table
-import jakarta.persistence.Id
+import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
-import jakarta.persistence.ManyToOne
-import jakarta.persistence.FetchType
+import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
 
 @Entity
 @Table(name = "rooms")
